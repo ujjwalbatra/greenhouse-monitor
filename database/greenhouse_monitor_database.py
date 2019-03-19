cc3711a30dd66833c68e3d0ca334e5e9a9e5748b
pavan1612 <33213778+pavan1612@users.noreply.github.com>
2019-03-19 16:10:54 +1100

    DB Check

--- database/greenhouse_monitor_database.py
+++ database/greenhouse_monitor_database.py
@@ -29,12 +29,22 @@ class GreenhouseMonitorDatabase(object):
     def create_tables(self):
 
         self.__cursor.execute('''
-            CREATE TABLE IF NOT EXISTS notification_confirmation (id INTEGER PRIMARY KEY AUTOINCREMENT, date DATE DEFAULT CURRENT_DATE NOT NULL , notification_sent INTEGER);
+            CREATE TABLE IF NOT EXISTS notification_confirmation (date DATE DEFAULT CURRENT_DATE NOT NULL , notification_sent INTEGER);
         ''')
 
         self.__cursor.execute('''
-                    CREATE TABLE IF NOT EXISTS sensor_data (id INTEGER PRIMARY KEY AUTOINCREMENT, notification_confirmation_id INTEGER, temperature REAL, humidity REAL, time_recorded TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL, FOREIGN KEY(notification_confirmation_id) REFERENCES notification_confirmation);
+                    CREATE TABLE IF NOT EXISTS sensor_data (id INTEGER PRIMARY KEY AUTOINCREMENT, temperature REAL, humidity REAL, time_recorded TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL);
         ''')
 
     def insert_to_db(self, temperature: float, humidity: float):
-        pass
+        print("Works")
+        print(temperature)
+        self.__cursor.execute("INSERT INTO sensor_data (temperature, humidity) VALUES (?,?);",(temperature,humidity))
+
+    def query_to_db(self):
+        self.__cursor.execute("SELECT * FROM sensor_data; ")
+ 
+        rows = self.__cursor.fetchall()
+        print("Works")
+        for row in rows:
+            print(row)
--- monitorAndNotify.py
+++ monitorAndNotify.py
@@ -5,6 +5,8 @@ from database import greenhouse_monitor_database
 db = greenhouse_monitor_database.GreenhouseMonitorDatabase()
 
 db.create_tables()
+db.insert_to_db(25,35)
+db.query_to_db()
 
 
 
