4b6b26b9b02704dcfb1d20561e9613eb930fc4e1
Ujjwal Batra <ujjwalbatra2509@gmail.com>
2019-03-23 18:21:06 +1100

    access_token set to config

--- notification/pushbullet.py
+++ notification/pushbullet.py
@@ -7,13 +7,17 @@ class PushBullet(object):
         'Content-Type': 'application/json',
     }
     __notification_type = 'note'
-    __data: str
+    __access_token: str
 
     def __init__(self, title, body):
         self.__data = json.dumps({"type": self.__notification_type, "title": title, "body": body})
+        self.__initialise_pushbullet_access_token()
 
     def send_notification(self):
-        response = requests.post('https://api.pushbullet.com/v2/pushes', headers=self.__headers, data=self.__data,
-                                 auth=('o.irAFuzgjn6N2TshmJm5ZApQxWXddQfX9', ''))
+        requests.post('https://api.pushbullet.com/v2/pushes', headers=self.__headers, data=self.__data,
+                      auth=(self.__access_token, ''))
 
-
+    def __initialise_pushbullet_access_token(self):
+        with open('config.json') as json_file:
+            data = json.load(json_file)
+            self.__access_token = data['pushbullet']['access_token']
