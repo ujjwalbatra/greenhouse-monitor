cc4e1d0e65f2936d574579a9c3343705199f3b75
pavan1612 <33213778+pavan1612@users.noreply.github.com>
2019-03-25 20:55:43 +1100

    Bluetooth pushing notification on pushbullet

---
+++ greenhouseBluetooth.py
@@ -0,0 +1,67 @@
+from notification.pushbullet import PushBullet
+import threading
+import json
+from database.greenhouse_monitor_database import GreenhouseMonitorDatabase
+import bluetooth
+
+
+class BlueToothNotify:
+
+    def check_temp_humidity(self, range_, rows):
+        line = {
+            "Min_temp_diff": 0,
+            "Max_temp_diff": 0,
+            "Min_humidity_diff": 0,
+            "Max_humidity_diff": 0
+        }
+        db = GreenhouseMonitorDatabase()
+        line["Min_temp_diff"] = db.get_today_min_temp()[0]
+        line["Max_temp_diff"] = db.get_today_max_temp()[0]
+        line["Min_humidity_diff"] = db.get_today_min_humidity()[0]
+        line["Max_humidity_diff"] = db.get_today_max_humidity()[0]
+        self.notify(line)
+
+    def notify(self, line):
+        status = "OK"
+        reasons = []
+        if line["Min_temp_diff"] != 0:
+            status = "Bad"
+            reasons.append("%s less than the minimum temperature. " % line["Min_temp_diff"])
+        if line["Max_temp_diff"] != 0:
+            status = "Bad"
+            reasons.append("%s more than the maximum temperature. " % line["Max_temp_diff"])
+        if line["Min_humidity_diff"] != 0:
+            status = "Bad"
+            reasons.append("%s less than the minimum humidity. " % line["Min_humidity_diff"])
+        if line["Max_humidity_diff"] != 0:
+            status = "Bad"
+            reasons.append("%s less than the maximum humidity. " % line["Max_humidity_diff"])
+
+        reason = ''.join(reasons)
+
+        if status == "Bad":
+            pb = PushBullet("Warning!", reason)
+            pb.send_notification()
+
+
+class GreenHouseBluetooth(threading.Thread):
+    def run(self):
+        nearby_devices = bluetooth.discover_devices(lookup_names=True)
+        trusted_devices = "Galaxy_S10"
+
+        for addr, name in nearby_devices:
+            if name == trusted_devices:
+                notify = BlueToothNotify()
+                db = GreenhouseMonitorDatabase()
+
+                rows = db.query_today()
+                notify.check_temp_humidity(self.get_range(), rows)
+
+    def get_range(self):
+        with open('config.json') as json_file:
+            data = json.load(json_file)
+            return data['data_range']
+
+
+t = GreenHouseBluetooth
+t.start()
