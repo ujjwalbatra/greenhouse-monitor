4f2cba5e55e067a67c04c5a5f741d1228f591937
pavan1612 <33213778+pavan1612@users.noreply.github.com>
2019-04-05 22:03:59 +1100

    Bluetooth paired devices notify

--- greenhouse_bluetooth.py
+++ greenhouse_bluetooth.py
@@ -3,6 +3,8 @@ import threading
 import json
 from database.greenhouse_monitor_database import GreenhouseMonitorDatabase
 import bluetooth
+import subprocess as sp
+import time
 
 
 class BlueToothNotify:
@@ -34,33 +36,38 @@ class BlueToothNotify:
         reasons = []
         if line["Min_temp_diff"] != 0:
             status = "Bad"
-            reasons.append("%s less than the minimum temperature. " % line["Min_temp_diff"])
+            reasons.append('\n%s less than the minimum temperature. ' % line["Min_temp_diff"])
         if line["Max_temp_diff"] != 0:
             status = "Bad"
-            reasons.append("%s more than the maximum temperature. " % line["Max_temp_diff"])
+            reasons.append("\n%s more than the maximum temperature. " % line["Max_temp_diff"])
         if line["Min_humidity_diff"] != 0:
             status = "Bad"
-            reasons.append("%s less than the minimum humidity. " % line["Min_humidity_diff"])
+            reasons.append("\n%s less than the minimum humidity. " % line["Min_humidity_diff"])
         if line["Max_humidity_diff"] != 0:
             status = "Bad"
-            reasons.append("%s more than the maximum humidity. " % line["Max_humidity_diff"])
+            reasons.append("\n%s more than the maximum humidity. " % line["Max_humidity_diff"])
 
         reason = ''.join(reasons)
         if status == "Bad":
-            pb = PushBullet("Warning!", reason)
+            pb = PushBullet("Warning! Values out of range.", reason)
             pb.send_notification()
 
 
 class GreenHouseBluetooth(threading.Thread):
     def run(self):
-        nearby_devices = bluetooth.discover_devices(lookup_names=True)
-        trusted_devices = "Galaxy_S10"
+        p = sp.Popen(["bt-device", "--list"], stdin=sp.PIPE, stdout=sp.PIPE, close_fds=True)
+        (stdout, stdin) = (p.stdout, p.stdin)
 
-        for addr, name in nearby_devices:
-            if name == trusted_devices:
-                notify = BlueToothNotify()
-
-                notify.check_temp_humidity(self.get_range())
+        data = stdout.readlines()
+        for devices in data:
+            devices = devices.decode("utf-8")
+            device = devices[devices.find('(') + 1:devices.find(')')]
+            nearby_devices = bluetooth.discover_devices(lookup_names=True)
+            trusted_devices = device
+            for addr, name in nearby_devices:
+                if addr == trusted_devices:
+                    notify = BlueToothNotify()
+                    notify.check_temp_humidity(self.get_range())
 
     def get_range(self):
         with open('config.json') as json_file:
@@ -69,5 +76,7 @@ class GreenHouseBluetooth(threading.Thread):
 
 
 if __name__ == '__main__':
-    bluetooth_notifications = GreenHouseBluetooth()
-    bluetooth_notifications.start()
+    while True:
+        bluetooth_notifications = GreenHouseBluetooth()
+        bluetooth_notifications.start()
+        time.sleep(3600)
