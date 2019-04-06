b61470de50d3df71088f7d2171af1eb28fa1d3ab
pavan1612 <33213778+pavan1612@users.noreply.github.com>
2019-04-06 21:17:37 +1100

    Notification fixed

--- greenhouse_bluetooth.py
+++ greenhouse_bluetooth.py
@@ -1,7 +1,7 @@
 from notification.pushbullet import PushBullet
 import threading
 import json
-from database.greenhouse_monitor_database import GreenhouseMonitorDatabase
+import time
 import bluetooth
 import subprocess as sp
 import logging
@@ -14,7 +14,6 @@ class GreenHouseBluetooth(threading.Thread):
     def run(self):
         p = sp.Popen(["bt-device", "--list"], stdin=sp.PIPE, stdout=sp.PIPE, close_fds=True)
         (stdout, stdin) = (p.stdout, p.stdin)
-        db = GreenhouseMonitorDatabase()
         data = stdout.readlines()
         for devices in data:
             devices = devices.decode("utf-8")
@@ -29,9 +28,37 @@ class GreenHouseBluetooth(threading.Thread):
                     humidity = temperature_humidity.get_humidity()
                     detail='\n Temperature: %.2f *c' %temperature
                     detail+= '\n Humidity:%.2f *c ' %humidity
-                    pb = PushBullet("Current values", detail)
-                    pb.send_notification()
-                    MonitorAndNotify.monitor_and_notify(db)
+                    delta_temp = temperature_humidity.get_temperature_difference()
+                    delta_humidity = temperature_humidity.get_humidity_difference()
+                    reasons=MonitorAndNotify.get_notification_detail(delta_temp, delta_humidity)
+                    #reasons=GreenHouseBluetooth.get_notification_detail(delta_temp, delta_humidity)
+                    if reasons == '':
+                        try:
+                            pb = PushBullet("Current values", detail)
+                            pb.send_notification()
+                        except Exception as e:  # any other exceptions
+                            logging.warning(e.__str__() + " " + datetime.now().__str__())
+                    else:
+                        try:
+                            pb = PushBullet("Current values", detail+reasons)
+                            pb.send_notification()
+                        except Exception as e:  # any other exceptions
+                            logging.warning(e.__str__() + " " + datetime.now().__str__())
+
+    @staticmethod
+    def get_notification_detail(delta_temperature, delta_humidity):
+        detail = ''
+
+        if delta_temperature > 0:
+            detail = '\nTemperature above maximum temperature by %.2f *c' % (delta_temperature,)
+        elif delta_temperature < 0:
+            detail = '\nTemperature below minimum temperature by %.2f *c' % (-delta_temperature,)
+
+        if delta_humidity > 0:
+            detail += '\nHumidity above maximum Humidity by %.2f %%rH' % (delta_humidity,)
+        elif delta_humidity < 0:
+            detail += '\nHumidity below minimum Humidity by %.2f %%rH' % (-delta_humidity,)
+        return detail
 
     def get_range(self):
         with open('config.json') as json_file:
